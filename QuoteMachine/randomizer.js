c9594eae6dbfd7724178a6a4e798f990ec927669
LarryA96 <156938571+LarryA96@users.noreply.github.com>
2026-07-22 12:31:02 -0400

    Update randomizer.js

--- QuoteMachine/randomizer.js
+++ QuoteMachine/randomizer.js
@@ -1,1 +1,9 @@
+// Select the element from the DOM
+const newQuote = document.querySelector("#new-quote");
+const text = document.querySelector("#text");
+const author = document.querySelector("#author");
 
+// Attach the click event listener
+newQuote.addEventListener("click", () => {
+  text.innerHTML = "We did it Gravy!";
+});
